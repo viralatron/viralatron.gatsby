4d213734f3fb1817be785c255ec1eee9256434cf
Daniel Amaral e Silva <dan.amaralsilva@gmail.com>
2021-09-08 16:06:22 -0300

    assets + alteração no componente
    pasta de assets adicionada
    alterado componente para colocar monitor na tela

--- src/threejs/home.js
+++ src/threejs/home.js
@@ -1,5 +1,8 @@
-import React, { useRef, useState } from 'react';
-import { Canvas, useFrame } from '@react-three/fiber';
+import React, { Suspense, useRef, useState } from 'react';
+import { Canvas, useFrame, useLoader } from '@react-three/fiber';
+import { GLTFLoader } from 'three/examples/jsm/loaders/gltfloader';
+import { Scene } from 'three';
+
 
 const Box = props => {
     const mesh = useRef();
@@ -21,14 +24,35 @@ const Box = props => {
     );
 }
 
+// can this be turned into a base component for assets?
+const Monitor = () => {
+    const meshRef = useRef();
+    
+    useFrame(() => (meshRef.current.rotation.y -= 0.01));
+
+    const gltf = useLoader(GLTFLoader, "/assets/monitor.glb");
+
+    return (
+        <Suspense fallback={null}>
+            <primitive 
+            ref={meshRef}
+            object={gltf.scene} scale={.75}
+            />
+        </Suspense>
+    );
+}
+
 const HomeAnim = ({ }) => {
     return(
         <Canvas className={"canvas"} style={{ position: "fixed", zIndex: "-1" }}>
             <ambientLight />
             <pointLight position={[10, 10, 10]} />
-            <Box position={[-1, 0, 0]} />
+            {/* <Box position={[-1, 0, 0]} />
             <Box position={[0, 0, 0]} />
-            <Box position={[1, 0, 0]} />
+            <Box position={[1, 0, 0]} /> */}
+            <Suspense fallback={null}>
+                <Monitor position={[0, 0, 0]}/>
+            </Suspense>
         </Canvas>
     );
 
